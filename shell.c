e143b72da6a149348e44a5f63747420adc2e3bb8
Simcha Klein <sklein27@cs.binghamton.edu>
2022-12-05 17:46:57 -0500

    q1 done

--- shell.c
+++ shell.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <stdlib.h>
 
 /*
  * shell() {
@@ -20,19 +21,51 @@
 int main()
 {
 	char command[1024];
-	char *args[] = {&command[0], NULL}; 
+	pid_t pid;
 
 	while(1) {
-		printf("hi> "); //prompt
+		printf("> "); //prompt
 		if(fgets(command, 1023, stdin) == NULL) break; 
 		if(strcmp(command, "exit\n") == 0) break; 
 		
-		char *p = &command[0]; 
-		while(*p != '\n') p++; 
-		*p = '\0';
+		// Remove newline character at end 
+		command[strcspn(command, "\n")] = '\0';
+
+		// Split command into command and arguments using strtok (a string tokenizer)
+		// found https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
+    	char *command_name = strtok(command, " ");
+		char *args[1024]; 
+    	int num_args = 0;
+    	while (command_name != NULL) {
+
+      		args[num_args] = command_name;
+      		num_args++;
+      		command_name = strtok(NULL, " ");
+   		}
+
+		//if we get here, there is only the command to execute with no arguments
+		if (num_args == 1){
+			
+			args[num_args] = NULL;
+			if(!fork()) {
+			if(execvp(args[0], args) == -1) {
+				printf("Exec failed!\n"); 
+				perror(NULL); 
+			}
+			exit(0); 
+			}
+			else {
+				//this is the parent
+				wait(NULL); 
+			}
+		}
+		else{
+		//if we get here, execute command with arguments as normal
+			num_args++;
+			args[num_args] = NULL;
 
 		if(!fork()) {
-			if(execv(args[0], args) == -1) {
+			if(execvp(args[0], args) == -1) {
 				printf("Exec failed!\n"); 
 				perror(NULL); 
 			}
@@ -42,9 +75,7 @@ int main()
 			//this is the parent
 			wait(NULL); 
 		}
+		}
 	}
-
 	return 0; 
 }
-
-
